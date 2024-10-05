acd0027232f94597c6a3f5aa6621f03dbcf2a0aa
Tawanna <thol_16@hotmail.com>
2024-10-05 12:13:42 -0300

    project created

---
+++ src/main/java/com/hakkinenT/cinemamongo/CinemamongoApplication.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.cinemamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemamongoApplication.class, args);
+	}
+
+}
